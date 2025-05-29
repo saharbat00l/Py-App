9a3a6f3f59c654b03e9e6bbbc4c8615516be7745
Sahar Batool <bsehar912@gmail.com>
2025-05-28 17:29:45 -0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,12 +1,12 @@
 import os
 import json
-import app
+import cli_app  # Instead of 'app'
 
 def test_add_and_load():
     if os.path.exists("tasks.json"):
         os.remove("tasks.json")
 
-    app.add_task("Test Task 1")
-    tasks = app.load_tasks()
+    cli_app.add_task("Test Task 1")
+    tasks = cli_app.load_tasks()
     assert tasks[0]["task"] == "Test Task 1"
     assert not tasks[0]["done"]
